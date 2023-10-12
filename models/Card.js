4b9e9a8393b28f9239a345e68e9b7220b2128f30
fabien382 <fabien.nvn@gmail.com>
2023-10-12 16:20:21 +0200

    Update crad model

--- models/Card.js
+++ models/Card.js
@@ -4,8 +4,16 @@ const mongoose = require("mongoose");
 const cardSchema = new mongoose.Schema({
   title: { type: String, required: true },
   desc: String,
-  priority: { type: Number, default: 0 },
-  state: { type: String, enum: ["todo", "done"], default: "active" },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "todo",
+  },
+  state: {
+    type: String,
+    enum: ["todo", "inprogress", "done"],
+    default: "todo",
+  },
 });
 
 module.exports = mongoose.model("Card", cardSchema);
